c53da668d59ba3c027af2716ae43d5adb473f0ec
Michael Stallings <stall84@gmail.com>
2020-08-02 20:11:39 -0400

    Rendered Solarized's pallete with Flat List 'mapping component'

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
+import { SafeAreaView, StyleSheet, FlatList } from 'react-native';
 import ColorBox from './components/ColorBox';
+import SolarizeBox from './components/SolarizeBox';
+
+
+
+const COLORS = [
+  { colorName: 'Base03', hexCode: '#002b36' },
+  { colorName: 'Base02', hexCode: '#073642' },
+  { colorName: 'Base01', hexCode: '#586e75' },
+  { colorName: 'Base00', hexCode: '#657b83' },
+  { colorName: 'Base0', hexCode: '#839496' },
+  { colorName: 'Base1', hexCode: '#93a1a1' },
+  { colorName: 'Base2', hexCode: '#eee8d5' },
+  { colorName: 'Base3', hexCode: '#fdf6e3' },
+  { colorName: 'Yellow', hexCode: '#b58900' },
+  { colorName: 'Orange', hexCode: '#cb4b16' },
+  { colorName: 'Red', hexCode: '#dc322f' },
+  { colorName: 'Magenta', hexCode: '#d33682' },
+  { colorName: 'Violet', hexCode: '#6c71c4' },
+  { colorName: 'Blue', hexCode: '#268bd2' },
+  { colorName: 'Cyan', hexCode: '#2aa198' },
+  { colorName: 'Green', hexCode: '#859900' },
+];
+
 
 const App = () => {
 
   return (
     <SafeAreaView style={styles.safeArea}>
-      <ColorBox colorName='Cyan' colorHex='#2aa198' /> 
-      <ColorBox colorName='Blue'  colorHex='#268bd2' />
-      <ColorBox colorName='Magenta'  colorHex='#d33682' />
-      <ColorBox colorName='Orange'  colorHex='#cb4b16' />
+      {/* <ColorBox colorName="Cyan" colorHex="#2aa198" /> 
+      <ColorBox colorName="Blue"  colorHex="#268bd2" />
+      <ColorBox colorName="Magenta"  colorHex="#d33682" />
+      <ColorBox colorName="Orange"  colorHex="#cb4b16" /> */}
+
+      <FlatList 
+          data={COLORS}
+          keyExtractor={item => item}
+          renderItem={({item}) =>
+                     <SolarizeBox 
+                     colorName={item.colorName} 
+                     colorHex={item.hexCode} 
+                     />}
+      />
+
       
+
+      
+
+
+
     </SafeAreaView>
   )
 
 };
+
 
 
 const styles = StyleSheet.create({
---
+++ components/SolarizeBox.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const SolarizeBox = ({colorName, colorHex}) => {
+
+    // We're getting our color name and color hex code passed down from the App component above
+    // Setting those to style properties 'boxStyle' then calling those styles in the render function below
+
+    const boxStyle = {
+        backgroundColor: colorHex,
+        colorName: colorName
+    }
+        
+
+    return (
+        // Still a little confused as to why merely 'boxStyle' gets called/passed instead of 'boxStyle.backgroundColor'
+
+        <View style={[ styles.containerBox, boxStyle ]}>
+            <Text style={styles.text}>
+                {boxStyle.colorName}
+            </Text>
+        </View>
+    )
+}
+
+// Instantiate and define styling for individual color boxes using StyleSheet component
+
+const styles = StyleSheet.create({
+    containerBox: {
+        alignItems: 'center',
+        borderRadius: 15,
+        marginVertical: 10,
+        marginHorizontal: 20,
+        paddingHorizontal: 10,
+        paddingVertical: 10,
+        backgroundColor: 'grey'
+    },
+    text: {
+        color: 'white',
+    }
+});
+
+export default SolarizeBox;
