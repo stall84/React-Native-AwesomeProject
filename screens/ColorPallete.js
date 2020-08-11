30e19b33128245d1f6acaa37d4e8f2f10895ae95
Michael Stallings <stall84@gmail.com>
2020-08-10 20:55:11 -0400

    Refactored with React-Navigation to now pass the color props down through the navigation event. Added comments

--- screens/ColorPallete.js
+++ screens/ColorPallete.js
@@ -1,32 +1,19 @@
 import React from 'react';
-import { SafeAreaView, FlatList, StyleSheet } from 'react-native';
+import { SafeAreaView, FlatList, StyleSheet, Text } from 'react-native';
 import SolarizeBox from '../components/SolarizeBox';
 
 
-const COLORS = [
-    { colorName: 'Base03', hexCode: '#002b36' },
-    { colorName: 'Base02', hexCode: '#073642' },
-    { colorName: 'Base01', hexCode: '#586e75' },
-    { colorName: 'Base00', hexCode: '#657b83' },
-    { colorName: 'Base0', hexCode: '#839496' },
-    { colorName: 'Base1', hexCode: '#93a1a1' },
-    { colorName: 'Base2', hexCode: '#eee8d5' },
-    { colorName: 'Base3', hexCode: '#fdf6e3' },
-    { colorName: 'Yellow', hexCode: '#b58900' },
-    { colorName: 'Orange', hexCode: '#cb4b16' },
-    { colorName: 'Red', hexCode: '#dc322f' },
-    { colorName: 'Magenta', hexCode: '#d33682' },
-    { colorName: 'Violet', hexCode: '#6c71c4' },
-    { colorName: 'Blue', hexCode: '#268bd2' },
-    { colorName: 'Cyan', hexCode: '#2aa198' },
-    { colorName: 'Green', hexCode: '#859900' },
-  ];
+
   
 
+// Receiving color props from above Home.js
+// React Navigation requires you pull props from the 'home/parent' component by using
+// destructuring individual route.params from the route prop
+const ColorPallete = ({ route }) => {
 
-const ColorPallete = () => {
+  // destructure individual props out of route params
+const { palleteName, colors } = route.params;
 
-    
 
     return (
 
@@ -34,14 +21,16 @@ const ColorPallete = () => {
       
 
             <FlatList 
-                data={COLORS}
+                data={colors}
                 keyExtractor={item => item.colorName}
                 renderItem={({item}) =>
                             <SolarizeBox 
                             colorName={item.colorName} 
                             colorHex={item.hexCode} 
                             />}
+                ListHeaderComponent={<Text style={styles.heading} >{palleteName}</Text>}
             />
+            
 
 
     </SafeAreaView>
@@ -54,6 +43,10 @@ const styles = StyleSheet.create({
       backgroundColor: 'white',
       flex: 1
     },
+    heading: {
+      fontSize: 18,
+      textAlign: 'center',
+    }
     
   })
 
